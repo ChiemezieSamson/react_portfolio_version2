17d5eb5f4b8be875d34daa82220ce67acd4e0a2c
Nebeolisa chiemezie Samson <daddygreenarrow44@gmail.com>
2023-11-04 16:39:58 +0900

    narrow the nav link paddings

--- src/Components/navigation/Navigation.jsx
+++ src/Components/navigation/Navigation.jsx
@@ -8,15 +8,15 @@ const Navigation = ({HandleShowNavLinks, showNavLinks, handleCloseSideBar}) => {
   
  
   return (
-    <nav className='grid grid-flow-col justify-between sm:py-5 max-h-24'>
+    <nav className='relative sm:py-5 max-h-24'>
 
       {/* Website Logo */}
       <Link to={"/"} className="block max-h-16 max-w-16 rounded-full overflow-clip mx-4 mt-5 sm:mt-0" onClick={handleCloseSideBar}>
-        <img loading='lazy'	src={Logo} alt="webSiteLogo"/>
+        <img loading='lazy'	src={Logo} alt="webSiteLogo" className='h-16 w-16 rounded-full'/>
       </Link>
 
-      <ul className={`bg-zinc-50/10 list-none supports-[backdrop-filter]:backdrop-blur pt-8 pb-6 ${showNavLinks ? "translate-x-0 visible opacity-100" : "translate-x-full invisible opacity-0"} 
-          sm:translate-x-0 sm:visible sm:opacity-100 sm:grid sm:grid-flow-col sm:px-6 sm:py-2.5 transition-all duration-300 ease-out`}>
+      <ul className={`bg-zinc-50/10 list-none supports-[backdrop-filter]:backdrop-blur py-2 ${showNavLinks ? "translate-x-0 visible opacity-100" : "translate-x-full invisible opacity-0"} 
+          sm:translate-x-0 sm:visible sm:opacity-100 sm:grid sm:grid-flow-col items-center sm:px-6 transition-all duration-300 ease-out sm:max-h-20 absolute right-0 sm:top-8 top-0`}>
 
         {navLinks.map((link) => {
 
@@ -34,7 +34,7 @@ const Navigation = ({HandleShowNavLinks, showNavLinks, handleCloseSideBar}) => {
         })}
       </ul>
 
-      <button onClick={(HandleShowNavLinks)} className='text-white sm:hidden text-2xl mt-5 cursor-pointer absolute top-2 right-2 bg-pink-300 hover:text-pink-300 p-0.5 shadow-sm shadow-black'>
+      <button onClick={(HandleShowNavLinks)} className='text-white sm:hidden text-2xl mt-5 cursor-pointer absolute top-0 right-2 bg-pink-300 hover:text-pink-300 p-0.5 shadow-sm shadow-black'>
         {showNavLinks ? <FaTimes className='gird items-center'/> : <FaBars className='gird items-center'/>}
       </button>
     </nav>
